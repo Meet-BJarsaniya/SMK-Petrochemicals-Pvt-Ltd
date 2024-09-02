00ed2adcb371891956d9dd678b4ac82e29426bdf
Meet-BJarsaniya <meet@sanskartechnolab.com>
2024-09-02 10:42:19 +0530

    JND po qc & CHA Quote

--- smk_scm/smk_scm/doctype/qc_warehouse_entry/qc_warehouse_entry.js
+++ smk_scm/smk_scm/doctype/qc_warehouse_entry/qc_warehouse_entry.js
@@ -2,37 +2,67 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("QC Warehouse Entry", {
-	before_save (frm) {
+    before_save(frm) {
         frm.doc.qc_item.forEach(row => {
             let po_qty = Number(row.purchase_order_qty);
             let received_qty = Number(row.received_qty);
             if (received_qty > po_qty) {
                 frappe.throw("Received Qty is more than PO Qty.")
+            } else if (received_qty < 0) {
+                frappe.throw("Received Qty can't be Negative.")
             }
         });
-	},
+    },
     refresh: function (frm) {
         frm.add_custom_button(__('Purchase Order'), function () {
-            open_purchase_order_dialog (frm);
+            open_purchase_order_dialog(frm);
         }, __('Get Items From'));
+    },
+    after_save(frm) {
+        let purchase_order = frm.doc.purchase_order;
+        let qc_items = frm.doc.qc_item;
+        let qc_items_dict = {};
+
+        // Create a dictionary mapping item_code to received_qty
+        qc_items.forEach(function (qc_item) {
+            qc_items_dict[qc_item.item_code] = qc_item.received_qty;
+        });
+
+        console.log(qc_items_dict);
+
+        frappe.call({
+            method: "smk_scm.smk_scm.doctype.qc_warehouse_entry.qc_warehouse_entry.update_purchase_order_items",
+            args: {
+                purchase_order: purchase_order,
+                qc_items_dict: qc_items_dict
+            },
+            callback: function (response) {
+                if (!response.exc) {
+                    frappe.msgprint("Purchase Order items updated successfully.");
+                    frm.refresh();
+                }
+            }
+        });
     }
 });
 
 frappe.ui.form.on("QC Warehouse Item", {
-	received_qty(frm, cdt, cdn) {
-        let row = locals [cdt] [cdn];
+    received_qty(frm, cdt, cdn) {
+        let row = locals[cdt][cdn];
         let po_qty = Number(row.purchase_order_qty);
         let received_qty = Number(row.received_qty);
         if (received_qty > po_qty) {
             frappe.throw("Received Qty is more than PO Qty.")
+        } else if (received_qty < 0) {
+            frappe.throw("Received Qty can't be Negative.")
         } else {
-            frappe.model.set_value(cdt, cdn, {'pending_qty':po_qty - received_qty});        
+            frappe.model.set_value(cdt, cdn, { 'pending_qty': po_qty - received_qty });
         }
-	},
+    },
 });
 
 
-function open_purchase_order_dialog (frm) {
+function open_purchase_order_dialog(frm) {
     let d = new frappe.ui.Dialog({
         title: 'Select Purchase Order',
         fields: [
@@ -49,7 +79,7 @@ function open_purchase_order_dialog (frm) {
             }
         ],
         primary_action_label: 'Get Items',
-        primary_action: function(data) {
+        primary_action: function (data) {
             get_items_from_purchase_order(frm, data.purchase_order);
             d.hide();
         }
@@ -85,18 +115,18 @@ function get_items_from_purchase_order(frm, purchase_order) {
             doctype: "Purchase Order",
             name: purchase_order
         },
-        callback: function(response) {
+        callback: function (response) {
             if (response.message.items) {
                 let items = response.message.items;
                 frm.set_value('purchase_order', response.message.name);
                 frm.clear_table('qc_item');
-                items.forEach(function(item) {
+                items.forEach(function (item) {
                     let new_row = frm.add_child('qc_item', {
                         item_code: item.item_code,
                         item_name: item.item_name,
                         item_group: item.item_group,
                         uom: item.uom,
-                        purchase_order_qty: item.qty,
+                        purchase_order_qty: item.qty - item.custom_qc_qty,
                     });
                 });
                 frm.refresh();
--- smk_scm/smk_scm/doctype/qc_warehouse_entry/qc_warehouse_entry.py
+++ smk_scm/smk_scm/doctype/qc_warehouse_entry/qc_warehouse_entry.py
@@ -8,11 +8,24 @@ from frappe.model.document import Document
 class QCWarehouseEntry(Document):
 	pass
 
+# @frappe.whitelist()
+# def get_items_from_po(purchase_order):
+# 	doc = frappe.get_doc('Purchase Order', purchase_order)
+# 	td = []
+# 	for row in doc.items:
+# 		# qty = row.balance_qty1 if row.balance_qty1 >= 0 else row.qty
+# 		tr = [row.item_code, row.item_name, qty, row.uom, doc.name]
+# 		td.append(tr)
+  
 @frappe.whitelist()
-def get_items_from_po(purchase_order):
-	doc = frappe.get_doc('Purchase Order', purchase_order)
-	td = []
-	for row in doc.items:
-		# qty = row.balance_qty1 if row.balance_qty1 >= 0 else row.qty
-		tr = [row.item_code, row.item_name, qty, row.uom, doc.name]
-		td.append(tr)
+def update_purchase_order_items(purchase_order, qc_items_dict):
+    # Ensure qc_items_dict is correctly parsed from JSON to a dictionary
+    if isinstance(qc_items_dict, str):
+        import json
+        qc_items_dict = json.loads(qc_items_dict)
+
+    purchase_order_doc = frappe.get_doc("Purchase Order", purchase_order)
+    for item in purchase_order_doc.items:
+        if item.item_code in qc_items_dict:
+            frappe.db.set_value("Purchase Order Item", item.name, "custom_qc_qty", qc_items_dict[item.item_code])
+    frappe.db.commit()
