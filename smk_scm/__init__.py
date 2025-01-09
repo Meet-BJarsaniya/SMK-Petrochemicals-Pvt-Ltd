c3f27cfde681dcd95628cd4287ca4198e12b9a41
Meet-BJarsaniya <meet@sanskartechnolab.com>
2025-01-09 18:44:33 +0530

    in QI validate_inspection_required skipped

--- smk_scm/__init__.py
+++ smk_scm/__init__.py
@@ -1,1 +1,11 @@
 __version__ = "0.0.1"
+
+
+from erpnext.stock.doctype.quality_inspection.quality_inspection import QualityInspection
+import frappe
+
+def custom_validate_inspection_required(self):
+    # Custom logic to skip validation or modify it
+    pass
+
+QualityInspection.validate_inspection_required = custom_validate_inspection_required
